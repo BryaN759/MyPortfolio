e2df193030bb006349dfe799c0bbd2f9d1dfb171
BryaN759 <khaledmushahed@gmail.com>
2024-08-02 20:46:49 +0600

    mern notes app

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -3,57 +3,64 @@ import Shakkhat from "../../assets/Shakkhat_profile.png";
 import TaskManager from "../../assets/taskmanagerproject.png";
 import JobPortal from "../../assets/Jobportal_home.png";
 import Insurance from "../../assets/insurance_home.png";
-
+import NotesApp from "../../assets/NotesApp.png";
 
 export const projectsData = [
-    {
-        id: 1,
-        image: KenaKata,
-        title: "KenaKata: Django E-commerce Website",
-        category: "Django",
-        link: "https://github.com/BryaN759/Django-Ecommerce",
-    },
-    {
-        id: 2,
-        image: Shakkhat,
-        title: "Shakkhat: Django and Vue3 Social Media Website",
-        category: "Django",
-        link: "https://github.com/BryaN759/Django-and-Vue3-Social-Media-Website",
-    },
-    {
-        id: 3,
-        image: TaskManager,
-        title: "Django Task Manager",
-        category: "Django",
-        link: "https://github.com/BryaN759/Django-Task-Manager-app",
-    },
-    {
-        id: 4,
-        image: JobPortal,
-        title: "NodeJs job portal Website",
-        category: "NodeJs",
-        link: "https://github.com/BryaN759/Internship-and-Job-Board",
-    },
-    {
-        id: 4,
-        image: Insurance,
-        title: "Php Insurance Management System",
-        category: "Php",
-        link: "https://github.com/BryaN759/Insurance-managemnet",
-    },
+  {
+    id: 1,
+    image: KenaKata,
+    title: "KenaKata: Django E-commerce Website",
+    category: "Django",
+    link: "https://github.com/BryaN759/Django-Ecommerce",
+  },
+  {
+    id: 2,
+    image: Shakkhat,
+    title: "Shakkhat: Django and Vue3 Social Media Website",
+    category: "Django",
+    link: "https://github.com/BryaN759/Django-and-Vue3-Social-Media-Website",
+  },
+  {
+    id: 3,
+    image: TaskManager,
+    title: "Django Task Manager",
+    category: "Django",
+    link: "https://github.com/BryaN759/Django-Task-Manager-app",
+  },
+  {
+    id: 4,
+    image: JobPortal,
+    title: "MERN stack Job Portal Website",
+    category: "MERN",
+    link: "https://github.com/BryaN759/Internship-and-Job-Board",
+  },
+  {
+    id: 5,
+    image: NotesApp,
+    title: "MERN stack Notes App",
+    category: "MERN",
+    link: "https://github.com/BryaN759/Internship-and-Job-Board",
+  },
+  {
+    id: 6,
+    image: Insurance,
+    title: "Php Insurance Management System",
+    category: "Php",
+    link: "https://github.com/BryaN759/Insurance-managemnet",
+  },
 ];
 
 export const projectsNav = [
-    {
-        name: "All",
-    },
-    {
-        name: "Django",
-    },
-    {
-        name: "NodeJs",
-    },
-    {
-        name: "Php",
-    },
+  {
+    name: "All",
+  },
+  {
+    name: "Django",
+  },
+  {
+    name: "MERN",
+  },
+  {
+    name: "Php",
+  },
 ];
