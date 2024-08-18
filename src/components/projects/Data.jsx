11f20da667960fa2841886490d168766522306ed
BryaN759 <khaledmushahed@gmail.com>
2024-08-18 21:51:10 +0600

    firebase-deploy script added

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -3,53 +3,53 @@ import Shakkhat from "../../assets/Shakkhat_profile.png";
 import TaskManager from "../../assets/taskmanagerproject.png";
 import JobPortal from "../../assets/Jobportal_home.png";
 import Insurance from "../../assets/insurance_home.png";
-import NotesApp from "../../assets/NotesApp.png";
+import NotesApp from "../../assets/notes_app.png";
 import BookingApp from "../../assets/booking_app.png";
 
 export const projectsData = [
   {
     id: 1,
+    image: NotesApp,
+    title: "NoteSnap: MERN Notes Website",
+    category: "MERN",
+    link: "https://github.com/BryaN759/MERN-Notes-App",
+    url: "https://mern-notes-web.onrender.com/",
+  },
+  {
+    id: 2,
+    image: BookingApp,
+    title: "BookMyStay: MERN Booking Platform",
+    category: "MERN",
+    link: "https://github.com/BryaN759/MERN-Booking-website",
+    url: "https://booking-website-s97m.onrender.com/",
+  },
+  {
+    id: 3,
+    image: JobPortal,
+    title: "JobConnect: MERN Job Portal",
+    category: "MERN",
+    link: "https://github.com/BryaN759/Internship-and-Job-Board",
+  },
+  {
+    id: 4,
     image: KenaKata,
     title: "KenaKata: Django E-commerce Website",
     category: "Django",
     link: "https://github.com/BryaN759/Django-Ecommerce",
   },
   {
-    id: 2,
+    id: 5,
     image: Shakkhat,
     title: "Shakkhat: Django & Vue3 Social Media",
     category: "Django",
     link: "https://github.com/BryaN759/Django-and-Vue3-Social-Media-Website",
   },
   {
-    id: 3,
+    id: 6,
     image: TaskManager,
     title: "Django Task Manager",
     category: "Django",
     link: "https://github.com/BryaN759/Django-Task-Manager-app",
-  },
-  {
-    id: 4,
-    image: JobPortal,
-    title: "MERN stack Job Portal Website",
-    category: "MERN",
-    link: "https://github.com/BryaN759/Internship-and-Job-Board",
-  },
-  {
-    id: 5,
-    image: NotesApp,
-    title: "MERN stack Notes App",
-    category: "MERN",
-    link: "https://github.com/BryaN759/MERN-Notes-App",
-    url: "https://mern-notes-web.onrender.com/",
-  },
-  {
-    id: 6,
-    image: BookingApp,
-    title: "MERN stack Booking App",
-    category: "MERN",
-    link: "https://github.com/BryaN759/MERN-Booking-website",
-    url: "https://booking-website-s97m.onrender.com/",
   },
   {
     id: 7,
@@ -65,10 +65,10 @@ export const projectsNav = [
     name: "All",
   },
   {
-    name: "Django",
+    name: "MERN",
   },
   {
-    name: "MERN",
+    name: "Django",
   },
   {
     name: "Php",
